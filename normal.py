2f443a054f11266344dd8ca0c66c1125e354a76b
Ruslan Zakharov <zakharov.ra@phystech.edu>
2022-03-10 12:38:37 +0300

    second attempt add show and gitignore

--- normal.py
+++ normal.py
@@ -5,3 +5,4 @@ x = np.linspace(0, size, size)
 y = np.random.normal(0, 1, size)
 show.my_plot(x, y)
 print("Hello, GitHub!")
+#checkpoint
--- show.py
+++ show.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-
+#checkpoint
 def my_plot(x, y):
     plt.plot(x, y, '.r')
     plt.show()
